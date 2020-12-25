fd41d000999ef37852e5deff756034b1a324c989
polemikal <68484486+polemikal@users.noreply.github.com>
2020-12-25 14:13:36 +0300

    Update Say.js

--- Commands/General/Say.js
+++ Commands/General/Say.js
@@ -30,7 +30,7 @@ class Say extends Command {
 
     async run(interaction, args) {
 
-        return await this.Bot.say(interaction, args[0].value);
+        return await this.Bot.send(interaction, args[0].value);
 
     }
 
